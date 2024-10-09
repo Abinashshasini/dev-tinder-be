d5a50ba1ec0d3b63629b1aa15e4a8a789452287a
newusername <=>
2024-10-09 12:16:15 +0530

    API work starts

--- src/controllers/request.controller.js
+++ src/controllers/request.controller.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcrypt');
 const { asyncHandler } = require('../utils/asyncHandler');
 const ConnectionRequest = require('../models/connectionRequest.js');
 const { ApiError } = require('../utils/apiError.js');
+const User = require('../models/user.js');
 const { ApiResponse } = require('../utils/apiResponse.js');
 
 /** Cookie option's for setting user cookies */
@@ -19,16 +19,40 @@ const handleLikeOrPass = asyncHandler(async (req, res) => {
   const toUserId = req.params.toUserId;
   const status = req.params.status;
   const allowedStatus = ['ignored', 'intrested'];
+
   if (!allowedStatus.includes(status)) {
     return res
       .status(401)
       .json(new ApiResponse(401, {}, `Invalid status type ${status}`));
   }
 
+  const toUser = await User.findById(toUserId);
+  if (!toUser) {
+    throw new ApiError('User not found!');
+  }
+
+  const isExistingConnectionRequest = ConnectionRequest.findOne({
+    $or: [
+      { fromUserId, toUserId },
+      { fromUserId: toUserId, toUserId: fromUserId },
+    ],
+  });
+
+  if (isExistingConnectionRequest) {
+    return res
+      .status(401)
+      .json(
+        new ApiResponse(
+          401,
+          {},
+          `Invalid connection request request already present`
+        )
+      );
+  }
+
   const connectionRequest = new ConnectionRequest({
     fromUserId,
     toUserId,
-    status,
   });
 
   const response = await connectionRequest.save();
